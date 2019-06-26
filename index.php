d95faa5883c4945e081d4259705fa9ed64784ccc
Theo <contact@divlo.fr>
2019-06-26 17:03:34 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Just Eat</title>
+    <link rel="icon" type="image/png" href="./img/burger.png"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <header class="header">
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <a class="navbar-brand" href="./index.php"><img class="logo-img" alt="" src="./img/burger.png"/>Just Eat</span></a>
+            <button class="navbar-toggler nav-button" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbar">
+              <ul class="navbar-nav">
+                <select class="custom-select">
+                    <li class="nav-item active">
+                    <option selected>Open this select menu</option>
+                    </li>
+                    <li class="nav-item">
+                    <option value="1">One</option>
+                    </li>
+                    <li class="nav-item">
+                    <option value="2">Two</option>
+                    </li>
+                    <li class="nav-item">
+                    <option value="3">Three</option>
+                    </li>
+                </select>
+              </ul>
+            </div>
+          </nav>
+    </header>
+   
+    <section id="central">
+        <p>Message Test</p>
+    </section>
+
+    <footer class="text-center align-middle">
+       <div class="container-fluid">
+          <div class="row">
+              <div class="col-12">
+                  <p class="footer-text">Footer Copyright</p>
+              </div>
+          </div>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
