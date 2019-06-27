d4a9fa589297b18660c203516039940e3e989b8a
Theo <contact@divlo.fr>
2019-06-27 08:49:03 +0200

    Mise en place de la partie front-end

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbar">
-              <ul class="navbar-nav">
+              <ul class="nav-select navbar-nav">
                 <select class="custom-select">
                     <li class="nav-item active">
                     <option selected>Open this select menu</option>
@@ -34,33 +34,33 @@
                     </li>
                 </select>
               </ul>
-              <div class="clear"></div>
+              
             </div>
           </nav>
     </header>
    
-    <section id="central">
-       <div class="container command-resto">
+    <section id="central" style="display: flex;align-items:center;justify-content:center;">
+       <div class="container command-resto" style="margin: auto;">
            <div class="row">
-               <div class="col-12">
-                   <h2 class="text-center">C'est bon de commander</h2>
-                   <h3 class="text-center">Faites-vous livrer les meilleurs restos près de chez vous avec Just Eat.</h3>
-                    <div class="container-fluid">
-                        <div class="row">
-                                <div class="col-4"></div>
-                                    <div class="col-6">
-                                        <form class="form-inline">
-                                            <div class="form-group mx-sm-3 mb-2">
-                                                <label for="inputAddress" class="sr-only">Entrez votre adresse</label>
-                                                <input type="inputAddress" class="form-control" id="inputAddress" placeholder="Entrez votre adresse">
-                                            </div>
-                                            <button type="submit" class="btn btn-primary mb-2">Je commande</button>
-                                        </form>
-                                    </div>
-                                <div class="col-2"></div>
+                <div class="col-12">
+                    <h2 class="title-command text-center">C'est bon de commander</h2>
+                    <h3 class="title-command text-center">Faites-vous livrer les meilleurs restos près de chez vous.</h3>
+                        <div class="container-fluid">
+                            <div class="row">
+                                    <div class="col-4"></div>
+                                        <div class="col-6">
+                                            <form class="form-inline">
+                                                <div class="form-group mx-sm-3 mb-2">
+                                                    <label for="inputAddress" class="sr-only">Entrez votre adresse</label>
+                                                    <input type="inputAddress" class="form-control" id="inputAddress" placeholder="Entrez votre adresse">
+                                                </div>
+                                                <button type="submit" class="btn btn-primary mb-2">Je commande</button>
+                                            </form>
+                                        </div>
+                                    <div class="col-2"></div>
+                            </div>
                         </div>
-                    </div>
-               </div>
+                </div>
            </div>
        </div>
     </section>
@@ -69,10 +69,7 @@
        <div class="container">
            <div class="row">
                <div class="col-12">
-                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut nibh id nisi volutpat tristique sit amet commodo dui. Integer euismod sed nunc vel vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut vel volutpat ipsum. Fusce neque tortor, facilisis quis ligula ut, tristique dapibus mauris. Morbi interdum facilisis ipsum sed vulputate. Cras nisl libero, dapibus eu sagittis quis, egestas at justo. Cras hendrerit sit amet elit in egestas. Ut aliquam fermentum justo sit amet porta. Vestibulum lectus nibh, vulputate non fringilla malesuada, volutpat eget urna. Fusce vehicula facilisis faucibus. Praesent efficitur elit et arcu interdum blandit. Suspendisse ac purus id elit tristique sagittis id id arcu. Etiam a feugiat ex. Etiam vitae lacus quis quam aliquam vestibulum at cursus turpis.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut nibh id nisi volutpat tristique sit amet commodo dui. Integer euismod sed nunc vel vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut vel volutpat ipsum. Fusce neque tortor, facilisis quis ligula ut, tristique dapibus mauris. Morbi interdum facilisis ipsum sed vulputate. Cras nisl libero, dapibus eu sagittis quis, egestas at justo. Cras hendrerit sit amet elit in egestas. Ut aliquam fermentum justo sit amet porta. Vestibulum lectus nibh, vulputate non fringilla malesuada, volutpat eget urna. Fusce vehicula facilisis faucibus. Praesent efficitur elit et arcu interdum blandit. Suspendisse ac purus id elit tristique sagittis id id arcu. Etiam a feugiat ex. Etiam vitae lacus quis quam aliquam vestibulum at cursus turpis.
-                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut nibh id nisi volutpat tristique sit amet commodo dui. Integer euismod sed nunc vel vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut vel volutpat ipsum. Fusce neque tortor, facilisis quis ligula ut, tristique dapibus mauris. Morbi interdum facilisis ipsum sed vulputate. Cras nisl libero, dapibus eu sagittis quis, egestas at justo. Cras hendrerit sit amet elit in egestas. Ut aliquam fermentum justo sit amet porta. Vestibulum lectus nibh, vulputate non fringilla malesuada, volutpat eget urna. Fusce vehicula facilisis faucibus. Praesent efficitur elit et arcu interdum blandit. Suspendisse ac purus id elit tristique sagittis id id arcu. Etiam a feugiat ex. Etiam vitae lacus quis quam aliquam vestibulum at cursus turpis.
-                   </p>
+                   <p></p>
                </div>
            </div>
        </div>
@@ -82,7 +79,7 @@
        <div class="container-fluid">
           <div class="row">
               <div class="col-12">
-                  <p class="footer-text"><span style="padding-top: 0;">Footer Copyright</span></p>
+                  <p class="footer-text"><span>Footer Copyright</span></p>
               </div>
           </div>
       </div>
