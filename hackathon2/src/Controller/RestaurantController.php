172501e2b49b7e87cfaaf5a3186c2b941fd13c11
Manuel Konrath <manuel.konrath@gmail.com>
2019-06-27 19:53:53 +0200

    appel de la base page resto

--- hackathon2/src/Controller/RestaurantController.php
+++ hackathon2/src/Controller/RestaurantController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Resto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundel\FrameworkBundle\Controller\Controller;
+
 
 class RestaurantController extends AbstractController
 {
@@ -13,5 +17,25 @@ class RestaurantController extends AbstractController
     public function index()
     {
         return $this->render('restaurant/index.html.twig');
+    }
+
+
+    /**
+     * @Route("/restaurant/{name<^[a-zA-Z0-9-]+$>}", defaults={"name" = null},
+     *  name="showRestaurantByName")
+     * @return Response
+     **/
+    public function showRestaurantByName($name)
+    {
+        $restaurant = $this->getDoctrine()->getRepository(Resto::class)
+            ->findOneBy(['Name'=>$name]);
+        if(!$restaurant) {
+            throw $this
+                ->createNotFoundException('No restaurant name send ');
+        }
+
+        return $this->render('restaurant/index.html.twig', ['restaurant'=>$restaurant]);
+
+
     }
 }
