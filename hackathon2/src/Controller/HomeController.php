7fc1314bb483ec49e4633039544ce77fae2312eb
Adrien <adrien.mechin@gmail.com>
2019-06-28 10:45:32 +0200

    commit

--- hackathon2/src/Controller/HomeController.php
+++ hackathon2/src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Service\readCookie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,7 +14,11 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-        ]);
+        return $this->render('home/test.html.twig', [
+            ]);
     }
+
+
+
+
 }
