cf76035023c77c8687a53430dfd9c44616ae66f7
Manuel Konrath <manuel.konrath@gmail.com>
2019-06-28 12:23:45 +0200

    start session

--- hackathon2/src/Controller/HomeController.php
+++ hackathon2/src/Controller/HomeController.php
@@ -10,18 +10,31 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Service\readCookie;
 
 class HomeController extends AbstractController
 {
+
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+
     /**
      * @Route("/", name="home")
      */
     public function index(TagScraper $scraper, NomResto $nomResto, EntityManagerInterface $en)
     {
-        $logo = $nomResto->logoResto();
-        $tags = $scraper->getAllTagsByRestaurants();
-        $description = $nomResto->descriptionResto();
-        $nameResto = $nomResto->nomResto();
+        $this->session->remove('tags');
+
+        $arraySortCookie = readCookie::CookMyCookie();
+
+        $this->session->set('tags', $arraySortCookie);
+
         $toutResto = $en->getRepository(Resto::class)->findAll();
 
         return $this->render('home/index.html.twig', [
